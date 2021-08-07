8d43b0c4956a98b988d7f4f251f44fa00f2da804
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-07 07:30:59 +0530

    Update kernel_setup.sh

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -9,8 +9,6 @@
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sweet.git -b arrow-11.0
 cd android_kernel_xiaomi_sweet
 
-wget https://raw.githubusercontent.com/phoenix-1708/delta_kernel/main/kernel_build.sh
-source kernel_build.sh
 
 # Config
 CURRENT_DIR="$(pwd)"
@@ -36,4 +34,7 @@ clone_tc
 clone_ak
 
 # Run build script
-. ${CURRENT_DIR}/kernel_build.sh
+# . ${CURRENT_DIR}/kernel_build.sh
+wget https://raw.githubusercontent.com/phoenix-1708/delta_kernel/main/kernel_build.sh
+source kernel_build.sh
+
