502e262811c016e6ce686d429201ff9217586c84
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-09 09:53:18 +0530

    Update kernel_setup.sh

--- kernel_setup.sh
+++ kernel_setup.sh
@@ -25,19 +25,19 @@ KERNEL_DIR="$CURRENT_DIR"
 AK_REPO="https://github.com/TheStaticDesign/AnyKernel3"
 AK_BRANCH="sweet"
 AK_DIR="$HOME/AnyKernel3"
-TC_DIR="$HOME/proton-clang"
-# TC_DIR="$HOME/linux-x86"
-# TC_DIR1="$HOME/aarch64-linux-android-4.9"
-# TC_DIR2="$HOME/arm-linux-androideabi-4.9"
+# TC_DIR="$HOME/proton-clang"
+TC_DIR="$HOME/linux-x86"
+TC_DIR1="$HOME/aarch64-linux-android-4.9"
+TC_DIR2="$HOME/arm-linux-androideabi-4.9"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 # End Config
 
 # clone_tc - clones proton clang to TC_DIR
 clone_tc() {
-	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
-	# git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 $TC_DIR
-	# git clone https://github.com/stormbreaker-project/aarch64-linux-android-4.9.git $TC_DIR1
-	# git clone https://github.com/stormbreaker-project/arm-linux-androideabi-4.9.git $TC_DIR2
+	# git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+	git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 $TC_DIR
+	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 $TC_DIR1
+	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 $TC_DIR2
 }
 
 # Clones anykernel
